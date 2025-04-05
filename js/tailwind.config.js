597f31169f771cae4ca4f5d26c1265a0c443243f
Marco <marco.cremaschi@unimib.it>
2025-04-06 01:04:48 +0200

    create new files

---
+++ js/tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#4F46E5',
+                secondary: "#F97316",
+                accent: '#A3E635',
+            },
+            fontFamily: {
+                'sans': ['TT Hoves SL', 'sans-serif'],
+                'mono': ['JetBrains Mono', 'monospace'],
+            },
+        },
+    },
+} 
